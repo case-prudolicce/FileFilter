dffe9d10ff3c81bea4a68426402ace23a32f8dc8
case-prudolicce <jeremie.simard.rip@gmail.com>
2021-06-29 22:09:41 -0400

    - Fixed md5_from_file - Moved code to make_hashfile - Added move_file and sort_files (With python-magick) - Implemented ArgParser

--- FileFilter.py
+++ FileFilter.py
@@ -3,97 +3,160 @@ import re
 import hashlib
 import os
 import sys
+import argparse
+import magic
 
 #function to make hash from files, which is read by blocks to avoid overloading ram
 def md5_from_file(f,bs=2**20):
 	h = hashlib.md5()
 	while True:
-		data = file.read(bs)
+		data = f.read(bs)
 		if not data:
 			break
 		h.update(data)
 	return h.hexdigest()
 
-#Check if we have 4 args (4th arg will be sort_table)
-#if not len(sys.argv) == 4:
-#	sys.exit()
-
-#Check if we have 3 args (Filefilter.py, Folder to filter, Folder to compare against)
-if not len(sys.argv) == 3:
-	sys.exit()
-
-#Get first and second arguments (for target and comparison folder)
-Target = sys.argv[1]
-Destination = sys.argv[2]
-
-#check if the .hashes exist, if it does prompt user to use it or overwrite it
-Destination = os.path.abspath(Destination)
-m = re.search('^(.*/.*/).*$',Destination)
-hs = m.group(1)
-if os.path.isfile(hs+".hashes"):
-	v = input(hs+".hashes Already exists. Do you want to use it instead of overwriting it (Y/n)?")
-	if v == "n": #Overwritting, same as writting
-		hashes = open(hs+".hashes",'a')
+#Function to create and write .hashes
+def make_hashfile(f, d):
+	pass
+	if os.path.isfile(f):
+		v = input(f+" Already exists. Do you want to use it instead of overwriting it (Y/n)?")
+		if v == "n": #Overwritting, same as writting
+			hashes = open(f,'w')
+			
+			for sd,d,F in os.walk(d):
+				for filename in F:
+					filepath = sd + os.sep + filename
+					#print(filepath)
+					file = open(filepath,mode='rb')
+					h = md5_from_file(file)+"\n"
+					file.close()
+					hashes.write(h)
+			hashes.close()
 		
-		for sd,d,f in os.walk(Destination):
-			for filename in f:
+	else: #Writting
+	#Open a file named .hashes in the folder directly outside of Destination
+		hashes = open(f,'w')
+		
+	#Open all files in Destination (Files to compare targets against)
+		for sd,d,F in os.walk(d):
+			for filename in F:
 				filepath = sd + os.sep + filename
-				print(filepath)
+				#print(filepath)
 				file = open(filepath,mode='rb')
+				#Add 
 				h = md5_from_file(file)+"\n"
 				file.close()
+	#For each file, make a hash of it and write it in .hashes
 				hashes.write(h)
 		hashes.close()
-	
-else: #Writting
-#Open a file named .hashes in the folder directly outside of Destination
-	hashes = open(hs+".hashes",'a')
-	
-#Open all files in Destination (Files to compare targets against)
-	for sd,d,f in os.walk(Destination):
-		for filename in f:
-			filepath = sd + os.sep + filename
-			print(filepath)
-			file = open(filepath,mode='rb')
-			#Add 
-			h = md5_from_file(file)+"\n"
-			file.close()
-#For each file, make a hash of it and write it in .hashes
-			hashes.write(h)
-	hashes.close()
 
+#Function to moves a file to destination
+def move_file(f,d):
+	#print(f,"->",os.path.basename(f),"->",d+os.path.basename(f))
+	os.rename(f,d+os.path.basename(f))
+	return d+os.path.basename(f)
 
-#Reopen (or open if chose yes) hashes in read mode
-#Get all hashes from files, removes trailing newline and store in a data structure (list for now)
+#Function to sorts the files to Sort_Dest based on Sort_Loc
+def sort_files(ts,sl,sd):
+	sl = os.path.abspath(sl)
+	
+	sd = os.path.abspath(sd)
+	if os.path.isfile(sl):
+		table = {}
+		with open(sl,'r') as stable:
+			entry = stable.readline()
+			c = 1
+			while entry:
+				if not ( entry[:-1] == "" or entry[0] == "#"):
+					table[entry[:-1].split(':')[0]] = entry[:-1].split(':')[1]
+				entry = stable.readline()
+				c += 1
+	
+	for file in ts:
+		mime = magic.from_file(file, mime=True)
+		moved = False
+		for mt in table.keys():
+			if mime == mt:
+				move_file(file,sd+table[mime])
+				moved = True
+		if not moved:
+			n = move_file(file,sd+table['*'])
+
+#Configure argparser
+parser = argparse.ArgumentParser()
+parser.add_argument('target')
+parser.add_argument('-d','--destination')
+parser.add_argument('-H','--hashes')
+parser.add_argument('-s','--sort-loc')
+parser.add_argument('-S','--sort-dest')
+parser.add_argument('--no-sort',action='store_true')
+args = parser.parse_args()
+
+Target = args.target
+if args.destination == None and args.hashes == None:
+	print("You either need -d or -H")
+	sys.exit()
+
+if args.sort_loc == None:
+	#Default
+	#Sort_Loc == "default value"
+	Sort_Loc = "/usr/share/FileFilter/default_sort_table"
+	print(Sort_Loc)
+else:
+	Sort_Loc = args.sort_loc
+
+#Check if hashes hasn't been defined, if it didnt, make one outside destination
+if args.hashes == None:
+	Destination = os.path.abspath(args.destination)
+	m = re.search('^(.*/.*/).*$',Destination)
+	Hashes = m.group(1)+".hashes"
+	make_hashfile(Hashes,Destination)
+else:
+	Hashes = os.path.abspath(args.hashes)
+	m = re.search('^(.*/.*/).*$',Hashes)
+	Destination = m.group(1)
+	make_hashfile(Hashes,args.destination)
+
+#Sets Sort_Dest (Needed after Destination is defined) to -S or outside destination
+if args.sort_dest == None:
+	Sort_Dest = os.path.abspath(Destination)
+	m = re.search('^(.*/.*/).*$',Destination)
+	Sort_Dest = m.group(1)+"Sorted/"
+else:
+	Sort_Dest = args.sort_dest
+
+#Open hashes in read mode, get all hashes from files, removes trailing newline and stores them in a list
 hashlist = []
-with open(hs+".hashes",'r') as hashes:
+with open(Hashes,'r') as hashes:
 	h4sh = hashes.readline()
 	c = 1
 	while h4sh:
-		#print(h4sh[:-1])
 		hashlist.append(h4sh[:-1])
 		h4sh = hashes.readline()
 		c += 1
 
-#Open sorttable from $HOME/.config/filefilter/sort_table from the third argument
-#make sort dictionary
-
-#Open all file in Target (Files to check/remove) 
-#For each file, make a hash of it and compare it against .hashes
-for sd,d,f in os.walk(Target):
-	for filename in f:
+#Open all file in Target 
+#For each file, make a hash of it and compare it against the hash list
+to_sort = []
+for sd,d,F in os.walk(Target):
+	for filename in F:
 		filepath = sd + os.sep + filename
 		file = open(filepath,mode='rb')
-		#Add 
 		h = md5_from_file(file)
+		found = False
 		for comp in hashlist:
-#If theres a match, remove that file
+			#If theres a match, remove that file
 			if h == comp:
-				print("DUPLICATE DETECTED: " + filepath + "... Removing")
 				os.remove(filepath)
+				found = True
 				break
-			else:
-				pass #This will sort the file based on fileinfo
-#Otherwise, get file info
-#sort it based on sort_table
+			
+		if not found:
+			#Otherwise, get Add to the to_sort array
+			to_sort.append(filepath)
 		file.close()
+
+#Sort using Sort_Loc to Sort_Dest is --no-sort isn't specified
+if not (args.no_sort):
+	sort_files(to_sort,Sort_Loc,Sort_Dest)
